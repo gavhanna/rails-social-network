6dee8434f7c1d6b446e4817b91a7ff438d5c62a6
Gav Hanna <gavhanna@gmail.com>
2018-05-01 20:23:23 +0000

    added username to user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
-  def hello
-    render html: "hello, world!"
-  end
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+    protected
+
+        def configure_permitted_parameters
+            devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:username, :email, :password) }
+            devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:username, :email, :password, :current_password, :avatar) }
+        end
 end
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,14 @@
 class StaticPagesController < ApplicationController
+    before_action :authenticate_user!
   def home
   end
   
   def profile
+    if params[:id]
+      url_id = params[:id]
+      @user = User.find_by_id(url_id)
+    else
+      @user = current_user
+    end
   end
 end
