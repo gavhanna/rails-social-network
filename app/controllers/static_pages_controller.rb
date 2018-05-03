76d09662e6966a316ac0f0b1a3fd39f223356097
Gav Hanna <gavhanna@gmail.com>
2018-05-03 12:19:42 +0000

    added posts and comments, homepage and profile page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,11 +4,16 @@ class StaticPagesController < ApplicationController
   end
   
   def profile
+    @comment = Comment.new
+    @comments = Comment.all
     if params[:id]
       url_id = params[:id]
       @user = User.find_by_id(url_id)
     else
       @user = current_user
     end
+    
+    @userPosts = Post.where("user_id = ?", @user.id)
+    @userComments = Comment.where("user_id = ?", @user.id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  resources :comments
+  resources :posts
   devise_for :users
   get 'static_pages/home'
   get '/profile', to: 'static_pages#profile'
+  get '/profile/:id' => 'static_pages#profile'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
